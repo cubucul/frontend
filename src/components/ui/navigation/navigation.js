3dfeb773752f2ff9fee8140bd25f521e5dad0d80
Ilya Agafonov <cubucul@gmail.com>
2021-08-10 21:44:03 +0500

    Адаптирует навигацию

--- src/components/ui/navigation/navigation.js
+++ src/components/ui/navigation/navigation.js
@@ -6,22 +6,24 @@ import './navigation.css';
 const Navigation = ({ links }) => {
   return (
     <nav className="navigation">
-      <ul className="navigation__list">
-        {
-          links.map(({ label, path }) => {
-            return (
-              <li key={path}>
-                <NavLink
-                  className="navigation__link"
-                  activeClassName="navigation__link--active"
-                  to={path}
-                  exact
-                >{label}</NavLink>
-              </li>
-            );
-          })
-        }
-      </ul>
+      <div className="navigation__limiter">
+        <ul className="navigation__list">
+          {
+            links.map(({ label, path }) => {
+              return (
+                <li key={path}>
+                  <NavLink
+                    className="navigation__link"
+                    activeClassName="navigation__link--active"
+                    to={path}
+                    exact
+                  >{label}</NavLink>
+                </li>
+              );
+            })
+          }
+        </ul>
+      </div>
     </nav>
   );
 };
