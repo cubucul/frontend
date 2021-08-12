697a2cc790df0c43ef4015ed003447867575d674
Ilya Agafonov <cubucul@gmail.com>
2021-08-12 19:43:28 +0500

    Адаптирует шапку

--- src/components/podcast/podcast-head/podcast-head.js
+++ src/components/podcast/podcast-head/podcast-head.js
@@ -9,32 +9,29 @@ const PodcastHead = (props) => {
 
   return (
     <div className="podcast-head">
-      <div>
-        <img
-          className="podcast-head__image"
-          src={coverUrl600}
-          width="210"
-          height="210"
-          alt={title}
-        />
-        <SubscribeButton
-          subscribed={subscribed}
-          onSubscribe={onSubscribe}
-        />
+      <img
+        className="podcast-head__image"
+        src={coverUrl600}
+        width="210"
+        height="210"
+        alt={title}
+      />
+      <SubscribeButton
+        className="podcast-head__subscribe-button"
+        subscribed={subscribed}
+        onSubscribe={onSubscribe}
+      />
+      <div className="podcast-head__title">
+        <Heading size="h4">{title}</Heading>
       </div>
-      <div>
-        <div className="podcast-head__title">
-          <Heading size="h4">{title}</Heading>
-        </div>
-        <p className="podcast-head__author">{author}</p>
-        <a
-          className="podcast-head__link"
-          href={link}
-          target="_blank"
-          rel="noreferrer"
-        >{link}</a>
-        <p className="podcast-head__description">{summary}</p>
-      </div>
+      <p className="podcast-head__author">{author}</p>
+      <a
+        className="podcast-head__link"
+        href={link}
+        target="_blank"
+        rel="noreferrer"
+      >{link}</a>
+      <p className="podcast-head__description">{summary}</p>
     </div>
   );
 };
