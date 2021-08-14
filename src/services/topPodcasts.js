cdc5194bfde0af42f9ac4d956a0259cbe33e5cf8
Ilya Agafonov <cubucul@gmail.com>
2021-08-14 10:29:29 +0500

    Удаляет трансформацию данных

--- src/services/topPodcasts.js
+++ src/services/topPodcasts.js
@@ -1,18 +1,9 @@
 const baseUrl = process.env.REACT_APP_URL_API;
 
-const transformPodcastData = (podcast) => {
-  return {
-    id: podcast.id,
-    title: podcast.name,
-    author: podcast.artistName,
-    coverUrl100: podcast.artworkUrl100
-  };
-};
-
 const fetchData = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
-  return data.feed.results.map(transformPodcastData);
+  return data;
 };
 
 export const getPodcastByGenre = async (genre = 'all', count = 25) => {
