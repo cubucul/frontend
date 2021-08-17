09ccca9d5e2014bf1a09ab77df346a4b86f5b154
Ilya Agafonov <cubucul@gmail.com>
2021-08-17 09:38:03 +0500

    Настраивает отступы

--- src/components/ui/subhead/subhead.js
+++ src/components/ui/subhead/subhead.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import './subhead.css';
 
-const Subhead = ({ children }) => {
+const Subhead = ({ children, topSpace = false, bottomSpace = true }) => {
+  const subheadClass = classNames('subhead', {
+    'subhead--top-space': topSpace,
+    'subhead--bottom-space': bottomSpace
+  });
+
   return (
-    <div className="subhead">
+    <div className={subheadClass}>
       {children}
     </div>
   );
 };
 
 Subhead.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  topSpace: PropTypes.bool,
+  bottomSpace: PropTypes.bool
 };
 
 export default Subhead;
