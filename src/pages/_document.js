ea6b853293dd4842fc205b8aabbe08b028e8a135
Ilya Agafonov <cubucul@gmail.com>
2021-08-17 14:22:24 +0500

    Исправляет charset на charSet

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html className="page" lang="en">
         <Head>
-          <meta charset="utf-8" />
+          <meta charSet="utf-8" />
           <link rel="icon" href="/favicon.ico" />
           <meta name="theme-color" content="#000000" />
           <meta
