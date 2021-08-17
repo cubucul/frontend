d680a9a54633b748480bcef60a7f3aaf5a1243aa
Ilya Agafonov <cubucul@gmail.com>
2021-08-17 13:05:26 +0500

    Переносит мета-тэги

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,16 @@
+import Head from 'next/head';
 import '../styles/index.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0" />
+        <title>React Podcasts</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
