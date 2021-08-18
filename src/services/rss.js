6171531aa2275ebdb4b3fcfbb3758dcd86824cea
Ilya Agafonov <cubucul@gmail.com>
2021-08-18 15:24:17 +0500

    Изменяет api

--- src/services/rss.js
+++ src/services/rss.js
@@ -1,6 +1,11 @@
 import { fetchData } from './api';
 
 export const fetchPodcastPageData = async (podcastId) => {
-  const data = await fetchData(`/?podcastId=${podcastId}`);
+  const data = await fetchData(`/getPodcast/${podcastId}`);
+  return data;
+};
+
+export const fetchEpisodePageData = async (podcastId, episodeId) => {
+  const data = await fetchData(`/getEpisode/${podcastId}/${episodeId}`);
   return data;
 };
