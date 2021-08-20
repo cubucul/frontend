9303b2b8db78f138c523249cd1c8f877fe3ff4d6
Ilya Agafonov <cubucul@gmail.com>
2021-08-20 20:50:10 +0500

    Добавляет трэкинг событий

--- src/store.js
+++ src/store.js
@@ -12,7 +12,8 @@ if (persistedState) {
   };
 }
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 }) || compose;
 const store = createStore(rootReducer, preloadedState, composeEnhancers(
   applyMiddleware(thunk)
 ));
