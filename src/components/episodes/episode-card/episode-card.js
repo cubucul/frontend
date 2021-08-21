ecdabd71380a813a3c08abaeeb53982ad0cb99ed
Ilya Agafonov <cubucul@gmail.com>
2021-08-21 19:57:13 +0500

    Добавляет вариант для прослушанного эпизода

--- src/components/episodes/episode-card/episode-card.js
+++ src/components/episodes/episode-card/episode-card.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { isEpisodeArchived } from '../../../selectors/history';
 import { secondsToString } from '../../../utils/time';
 import { getEpisodeIndex } from '../../../utils/helpers';
 import PlayControl from '../../common/play-control';
@@ -21,6 +23,8 @@ const EpisodeCard = ({ episodeData, noImage, noPodcastLink }) => {
     number,
     episodeType
   } = episodeData;
+  const isArchived = useSelector((state) => isEpisodeArchived(state, episodeId));
+
   const titleClass = classNames('episode-card__title', {
     'episode-card__title--regular': noPodcastLink
   });
@@ -28,7 +32,8 @@ const EpisodeCard = ({ episodeData, noImage, noPodcastLink }) => {
   const episodeIndex = getEpisodeIndex(season, number, episodeType);
   const episodeCardClass = classNames('episode-card', {
     'episode-card--noindex': noImage && !episodeIndex,
-    'episode-cart--with-image': !noImage
+    'episode-cart--with-image': !noImage,
+    'episode-card--archived': isArchived
   });
 
   return (
@@ -56,6 +61,7 @@ const EpisodeCard = ({ episodeData, noImage, noPodcastLink }) => {
         {secondsToString(duration)}
       </span>
       <PlayControl
+        isArchived={isArchived}
         className="episode-card__control"
         selectedEpisodeData={{
           episodeId,
